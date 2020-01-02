0000ddba779a692d9936ba334a636aa3c86be94f
brittanymae01 <brittanymae01>
2020-01-02 10:02:48 -0800

    fixed wireframe

--- src/components/UploadPage2.jsx
+++ src/components/UploadPage2.jsx
@@ -4,14 +4,17 @@ const UploadTwo = () => {
   return (
     <div>
       <h3>Collector's Story</h3>
-      <input type='text' placeholder='What do you love about this collectable?' />
+      <input
+        type="text"
+        placeholder="What do you love about this collectable?"
+      />
       <h4>Are you willing to sell this item?</h4>
       <p>Other Curio users may contact you to discuss a potential sale</p>
       <button>Yes</button>
       <button>No</button>
       <button>Continue</button>
     </div>
-  )
+  );
 };
 
 export default UploadTwo;
