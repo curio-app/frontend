db2233d71f2b4e31cca6a3a35f9b8d7573c7f3ca
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-02 12:53:30 -0500

    upload page 1 wireframe complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import UploadOne from './components/UploadPage1'
+
 function App() {
   return (
     <div className="App" role="main">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="react logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <UploadOne />
     </div>
   );
 }
