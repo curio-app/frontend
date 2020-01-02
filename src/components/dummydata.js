1de907eec51e54b935037d68c84fb0d443d272f7
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-02 14:20:31 -0500

    dummydata.js added

---
+++ src/components/dummydata.js
@@ -0,0 +1,42 @@
+export const dummydata = [
+    {
+        "id" : 0,
+        "item_name": "ehrmagerd",
+        "img_url": "https://via.placeholder.com/150",
+        "likes": "like",
+        "collector_story": "It's awesome",
+        "description": "It's a thing"
+    },
+        {
+        "id" : 1,
+        "item_name": "ehrmagerd",
+        "img_url": "https://via.placeholder.com/150",
+        "likes": "like",
+        "collector_story": "It's awesome",
+        "description": "It's a thing"
+    },
+    {
+        "id" : 2,
+        "item_name": "ehrmagerd",
+        "img_url": "https://via.placeholder.com/150",
+        "likes": "like",
+        "collector_story": "It's awesome",
+        "description": "It's a thing"
+    },
+    {
+        "id" : 3,
+        "item_name": "ehrmagerd",
+        "img_url": "https://via.placeholder.com/150",
+        "likes": "like",
+        "collector_story": "It's awesome",
+        "description": "It's a thing"
+    },
+    {
+        "id" : 4,
+        "item_name": "ehrmagerd",
+        "img_url": "https://via.placeholder.com/150",
+        "likes": "like",
+        "collector_story": "It's awesome",
+        "description": "It's a thing"
+    }
+]
