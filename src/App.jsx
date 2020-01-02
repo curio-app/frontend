36a1fcccb4b25ac2e8e9e0dd1ae6f19dff7d3355
brittanymae01 <brittanymae01>
2020-01-02 10:15:20 -0800

    wireframe for upload page 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import UploadTwo from './components/UploadPage2';
+import UploadThree from './components/UploadPage3';
 
 function App() {
   return (
     <div className="App" role="main">
       <UploadTwo />
+      <UploadThree />
     </div>
   );
 }
