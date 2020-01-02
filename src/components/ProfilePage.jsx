ba2ebd6374d3a0c667fc9f2d62b271c82e3ba236
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-02 14:07:30 -0500

    profile page wireframe

---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://via.placeholder.com/150"/>
+            </div>
+            <div>
+                <h3>UsernamePlaceholder</h3>
+                <p>Total Collectible: <span>50</span></p>
+                <button>Add a new collectible</button>
+            </div>
+        </div>
+    )
+}
+
+export default ProfilePage;
