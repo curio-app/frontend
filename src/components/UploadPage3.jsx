6f2cbcae84327af074d993a0a3012eeb22034765
brittanymae01 <brittanymae01>
2020-01-02 13:05:06 -0800

    added form tags

--- src/components/UploadPage3.jsx
+++ src/components/UploadPage3.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 const UploadThree = () => {
   return (
     <div>
-      <h3>Description</h3>
-      <input
-        type="text"
-        placeholder="How would you describe this collectible"
-      />
-      <h3>Tags</h3>
-      <p>Keywords to help other users find your item while searching</p>
-      <button>Add Tag +</button>
-      <button>Finish</button>
+      <form>
+        <h3>Description</h3>
+        <input
+          type="text"
+          placeholder="How would you describe this collectible"
+        />
+        <h3>Tags</h3>
+        <p>Keywords to help other users find your item while searching</p>
+        <button>Add Tag +</button>
+        <button>Finish</button>
+      </form>
     </div>
   );
 };
