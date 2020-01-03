cd8af9c7f23ad164cfb7ced1cde9cb3920b82bca
Kevin Smith <keveightysev@gmail.com>
2020-01-02 22:27:52 -0600

    Appeased linter for Register

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
-const token = 'response.data.token';
-
-const Register = props => {
+const Register = ({ history }) => {
   const [user, setUser] = useState({
     email: '',
     password: '',
@@ -21,7 +20,7 @@ const Register = props => {
         .post('https://curi0.herokuapp.com/auth/register', user)
         .then(response => {
           localStorage.setItem('token', response.data.token);
-          props.history.push('/UserPage');
+          history.push('/UserPage');
         })
         .catch(err => console.log(err));
       console.log('SUCCESS!');
@@ -58,6 +57,12 @@ const Register = props => {
       </form>
     </div>
   );
+};
+
+Register.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
 export default Register;
