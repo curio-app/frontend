0be06d88d91a74537d23c744c2db2eeebbd3205a
Kevin Smith <keveightysev@gmail.com>
2020-01-03 11:27:36 -0600

    some adjustments

--- src/components/SingleItemPage.jsx
+++ src/components/SingleItemPage.jsx
@@ -1,96 +1,120 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import axiosWithAuth from './auth/axiosWithAuth';
+
 import './SingleItem.css';
 
-//singledummy data
-
-import axios from 'axios';
-
-const SingleItemPage = (props) => {
+const SingleItemPage = props => {
   const [itemData, setItemData] = useState({
-    name: "",
-    username: "",
-    story: "",
-    description: "",
+    name: '',
+    username: '',
+    story: '',
+    description: '',
     likes: [],
     tags: [],
   });
-  
-  useEffect(()=> {
-    axios.get(`https://curi0.herokuapp.com/collectibles/${props.match.params.id}`)
-    .then(res => setItemData(res.data))
-  }, [props.match.params.id])
-  console.log(itemData)
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    const { username } = JSON.parse(localStorage.getItem('user'));
+    axios
+      .get(`https://curi0.herokuapp.com/collectibles/${props.match.params.id}`)
+      .then(res => {
+        setItemData(res.data);
+        if (username) {
+          const likeCheck = res.data.likes.find(
+            user => user.username === username
+          );
+          if (likeCheck) {
+            setLiked(true);
+          }
+        }
+      });
+  }, [props.match.params.id, setItemData]);
+
+  const handleLike = e => {
+    e.preventDefault();
+    setItemData({
+      ...itemData,
+      likes: [...itemData.likes, { id: 1, username: 'username' }],
+    });
+    setLiked(true);
+  };
+
+  const handleUnlike = e => {
+    e.preventDefault();
+    setItemData({
+      ...itemData,
+      likes: [],
+    });
+    setLiked(false);
+  };
 
   return (
-    <>
-      <div className="single-item-wrapper">
-        <section className="single-item-header">
-          <div className="single-item-img">
-            <img alt="the product" src={itemData.imageUrl} />
-          </div>
-          <div className="single-item-content">
-            <h2>{itemData.name}</h2>
-            <p>
-              Shared by{' '}
-              <span className="text">
-                <strong>{itemData.username}</strong>
-              </span>
-            </p>
-            <p>
-              Liked{' '}
-              <span className="text">
-                <strong>{itemData.likes.length}</strong>
-              </span>{' '}
-              times
-            </p>
-            <button className="button like">
-              Like{' '}
-              <span>
-                <i class="far fa-heart"></i>
-              </span>
+    <main className="single-item-wrapper">
+      <section className="single-item-header">
+        <img alt="the product" src={itemData.imageUrl} />
+        <aside className="single-item-content">
+          <h2>{itemData.name}</h2>
+          <p>
+            Shared by&nbsp;
+            <Link to={`/profile/${itemData.username}`}>
+              {itemData.username}
+            </Link>
+          </p>
+          <p>
+            Liked&nbsp;
+            {itemData.likes.length}
+            &nbsp;times
+          </p>
+          {!liked ? (
+            <button type="button" className="button like" onClick={handleLike}>
+              Like&nbsp;
+              <i className="far fa-heart" />
             </button>
-            <button className="button liked">
-              Liked{' '}
-              <span>
-                // <i class="fas fa-heart"></i>
-              </span>
+          ) : (
+            <button
+              type="button"
+              className="button liked"
+              onClick={handleUnlike}
+            >
+              Liked&nbsp;
+              <i className="fas fa-heart" />
             </button>
-            <h3>Looking to purchase?</h3>
-            <div className="purchase-icons">
-              <i class="fab fa-amazon"></i>
-              <i class="fab fa-ebay"></i>
-              <i class="fab fa-etsy"></i>
-              <i class="far fa-envelope"></i>
-            </div>
-          </div>
-        </section>
+          )}
+          {/* <h3>Looking to purchase?</h3>
+          <div className="purchase-icons">
+            <i className="fab fa-amazon" />
+            <i className="fab fa-ebay" />
+            <i className="fab fa-etsy" />
+            <i className="far fa-envelope" />
+          </div> */}
+        </aside>
+      </section>
 
-        <div className="single-item-info">
-          <section className="story">
-            <h2>Collector's Story</h2>
-            <p>{itemData.story === null ? 'N/A' : `${itemData.story}`} </p>
-          </section>
-          <section className="description">
-            <h2>Description</h2>
-            <p>
-              {itemData.description === null
-                ? 'N/A'
-                : `${itemData.description}`}{' '}
-            </p>
-          </section>
-          <section className="tags">
-            <h2>Tags</h2>
-            {itemData.tags.length === 0 ? (
-              <span className="tag notag">N/A</span>
-            ) : (
-              itemData.tags.map(tag => (
-                <span className="tag withtag">{tag.name}</span>
-              ))
-            )}
-          </section>
-        </div>
-      </div>
-    </>
+      <section className="single-item-info">
+        <section className="story">
+          <h2>Collector&apos;s Story</h2>
+          <p>{itemData.story === null ? 'N/A' : `${itemData.story}`}</p>
+        </section>
+        <section className="description">
+          <h2>Description</h2>
+          <p>
+            {itemData.description === null ? 'N/A' : `${itemData.description}`}
+          </p>
+        </section>
+        <section className="tags">
+          <h2>Tags</h2>
+          {itemData.tags.length === 0 ? (
+            <p className="tag notag">N/A</p>
+          ) : (
+            itemData.tags.map(tag => <p className="tag withtag">{tag.name}</p>)
+          )}
+        </section>
+      </section>
+    </main>
   );
 };
 
