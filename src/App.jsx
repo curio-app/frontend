f4881f54c0019b6732798bf53756a4bac39a374e
Kevin Smith <keveightysev@gmail.com>
2020-01-02 21:53:00 -0600

    Appeased linter on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import LandingPage from './components/LandingPage';
 import { Route } from 'react-router-dom';
 
+import './App.css';
+
+import LandingPage from './components/LandingPage';
 import Login from './components/auth/Login';
 import Nav from './components/Nav';
-import './App.css';
 import Register from './components/auth/Register';
 
 function App() {
