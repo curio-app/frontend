94c92396cb3d5378a1400bd9195df85ad15303a0
Tim Griffith <grifmang@gmail.com>
2020-01-02 20:44:52 -0500

    url fix

--- src/components/auth/axiosWithAuth.js
+++ src/components/auth/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
   return axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://curi0.herokuapp.com',
     headers: {
       Authorization: token
     }
